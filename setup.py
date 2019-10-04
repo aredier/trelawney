13ca97c7eb3beb38ac2b4b648dd088ee462a60d8
aredier <antoine.redier2@gmail.com>
2019-10-04 16:14:48 +0200

    getting rid of 2.7 check

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Skander Kamoun",
     author_email='skander.kam2@gmail.com',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=3.5, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
