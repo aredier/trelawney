cdd24533a66f397846fa3e0f85124d479bc1ea30
Amélie Meurer <amelie.meurer@hec.edu>
2019-10-04 19:09:04 +0200

    v1 of local and global explainer

--- trelawney/shap_explainer.py
+++ trelawney/shap_explainer.py
@@ -23,23 +23,30 @@ class ShapExplainer(BaseExplainer):
     def fit(self, model: sklearn.base.BaseEstimator, x_train: pd.DataFrame, y_train: pd.DataFrame):
         self._model_to_explain = model
         if self._model_to_explain == LogisticRegression:
-            self._explainer = shap.LinearExplainer(self._model_to_explain, data = x_train, feature_dependence="independent")
+            self._explainer = shap.LinearExplainer(self._model_to_explain, data=x_train, feature_dependence="independent")
         elif self._model_to_explain in [DecisionTreeClassifier, RandomForestClassifier, XGBClassifier]:
-            self._explainer = shap.TreeExplainer(self._model_to_explain, data = x_train)
+            self._explainer = shap.TreeExplainer(self._model_to_explain, data=x_train)
         elif self._model_to_explain == models.Model:
-            self._explainer = shap.DeepExplainer(self._model_to_explain, data = x_train)
-        else :
+            self._explainer = shap.DeepExplainer(self._model_to_explain, data=x_train)
+        else:
             raise NotImplementedError('For now, this package has only been implemented for LogisticRegression,'
                                       'DecisionTreeClassifier, RandomForestClassifier, XGBClassifier and NN')
-"""
 
     def explain_local(self, x_explain: pd.DataFrame, n_cols: Optional[int] = None) -> List[Dict[str, float]]:
-        shap_values = self._explainer.shap_values(x_explain)
-        return shap_values
+        shap_values = self._explainer.shap_values()
+        res = []
+        for individual_sample in tqdm(x_explain.iterrows()):
+            individual_explanation = {col: np.mean(np.abs(col_values)) for col, col_values in zip(X_test.columns, shap_values_XGB_test[0].T)}
+            individual_explanation = sorted(individual_explanation.items(), key=operator.itemgetter(1),reverse=True)
+            res.append(dict(individual_explanation.as_list()))
+        return [shap_values, res]
 
     def feature_importance(self, x_explain: pd.DataFrame, n_cols: Optional[int] = None) -> Dict[str, float]:
-        pass
+        shap_dict = dict(zip(X_test.columns.to_list(), list(np.mean(abs(shap_values_XGB_test), axis = 0).tolist())))
+        kept_shap_bar_cols = dict(sorted(shap_dict.items(), key=lambda x: np.abs(x[1]), reverse=True,)[:N_COMPS])
 
+
+"""
     def graph_feature_importance(self, x_explain: pd.DataFrame, cols: List[str], n_cols: Optional[int] = None):
         pass
 
