02d55bde2609b395beb995aaf98cd45c24810ef8
Amélie Meurer <amelie.meurer@hec.edu>
2019-10-04 20:36:44 +0200

    adding function for local explanation

--- trelawney/shap_explainer.py
+++ trelawney/shap_explainer.py
@@ -3,10 +3,13 @@ from typing import List, Optional, Dict
 import pandas as pd
 import shap as shap
 import sklearn
+import numpy as np
+import operator
 from keras import models
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+from tqdm import tqdm
 from xgboost import XGBClassifier
 
 from trelawney.base_explainer import BaseExplainer
@@ -25,7 +28,7 @@ class ShapExplainer(BaseExplainer):
         self._model_to_explain = model
         self._model_type = type(self._model_to_explain)
         if self._model_type == LogisticRegression:
-            self._explainer = shap.LinearExplainer(self._model_to_explain, data=x_train, feature_dependence="independent")
+            self._explainer = shap.LinearExplainer(self._model_to_explain, data=x_train)
         elif self._model_type in [DecisionTreeClassifier, RandomForestClassifier, XGBClassifier]:
             self._explainer = shap.TreeExplainer(self._model_to_explain, data=x_train)
         elif self._model_type == models.Model:
@@ -33,15 +36,18 @@ class ShapExplainer(BaseExplainer):
         else:
             self._explainer = shap.KernelExplainer(self._model_to_explain, data=x_train)
 
-"""
     def explain_local(self, x_explain: pd.DataFrame, n_cols: Optional[int] = None) -> List[Dict[str, float]]:
-        shap_values = self._explainer.shap_values()
+        shap_values = self._explainer.shap_values(x_explain)
+        n_cols = n_cols or len(x_explain.columns)
         res = []
-        for individual_sample in tqdm(x_explain.iterrows()):
-            individual_explanation = {col: np.mean(np.abs(col_values)) for col, col_values in zip(X_test.columns, shap_values_XGB_test[0].T)}
-            individual_explanation = sorted(individual_explanation.items(), key=operator.itemgetter(1),reverse=True)
-            res.append(dict(individual_explanation.as_list()))
-        return [shap_values, res]
+        for individual_sample in tqdm(range(len(x_explain))):
+            individual_explanation = {col: np.mean(np.abs(col_values)) for col, col_values in zip(x_explain.columns, shap_values[individual_sample].T)}
+            individual_explanation = sorted(individual_explanation.items(), key=operator.itemgetter(1), reverse=True)[:n_cols]
+            res.append(dict(individual_explanation))
+        return res
+
+
+"""
 
     def feature_importance(self, x_explain: pd.DataFrame, n_cols: Optional[int] = None) -> Dict[str, float]:
         shap_dict = dict(zip(X_test.columns.to_list(), list(np.mean(abs(shap_values_XGB_test), axis = 0).tolist())))
