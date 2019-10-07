a08cc17f52ed2b76c03d1ad52ea13b5fcfaef04c
aredier <antoine.redier2@gmail.com>
2019-10-07 12:07:14 +0200

    add feature importance graph

--- trelawney/shap_explainer.py
+++ trelawney/shap_explainer.py
@@ -41,21 +41,27 @@ class ShapExplainer(BaseExplainer):
         return shap.KernelExplainer
 
     def fit(self, model: sklearn.base.BaseEstimator, x_train: pd.DataFrame, y_train: pd.DataFrame):
-        if isinstance(model, KerasClassifier):
-            # SHAP doesn't work with the sklearn wrappers of Keras
-            super().fit(model.model, x_train, y_train)
-        else:
-            super().fit(model, x_train, y_train)
-        self._explainer = self._find_right_explainer_class(self._model_to_explain)(self._model_to_explain,
-                                                                                  data=x_train.values)
+        super().fit(model, x_train, y_train)
+        if isinstance(self._model_to_explain, KerasClassifier):
+            self._explainer = self._find_right_explainer_class(self._model_to_explain.model)(
+                self._model_to_explain.model, data=x_train.values
+            )
+            return
+        self._explainer = self._find_right_explainer_class(self._model_to_explain)(
+            self._model_to_explain, data=x_train.values
+        )
+
+    def _get_shap_values(self, x_explain):
+        shap_values = self._explainer.shap_values(x_explain.values)
+        if isinstance(self._model_to_explain, KerasClassifier):
+            # for nn, shap creates a list of shap values for every input layer in the NN,
+            # we assume one input layer
+            return shap_values[0]
+        return shap_values
 
     def explain_local(self, x_explain: pd.DataFrame, n_cols: Optional[int] = None) -> List[Dict[str, float]]:
         super().explain_local(x_explain)
-        shap_values = self._explainer.shap_values(x_explain.values)
-        if isinstance(self._model_to_explain, keras.models.Model):
-            # for nn, shap creates a list of shap values for every input layer in the NN,
-            # we assume one input layer
-            shap_values = shap_values[0]
+        shap_values = self._get_shap_values(x_explain)
         n_cols = n_cols or len(x_explain.columns)
         res = []
         for individual_sample in tqdm(range(len(x_explain))):
@@ -74,7 +80,7 @@ class ShapExplainer(BaseExplainer):
         return res
 
     def feature_importance(self, x_explain: pd.DataFrame, n_cols: Optional[int] = None) -> Dict[str, float]:
-        shap_values = self._explainer.shap_values(x_explain)
+        shap_values = self._get_shap_values(x_explain)
         shap_dict = dict(zip(x_explain.columns.to_list(), list(np.mean(abs(shap_values), axis=0).tolist())))
         kept_shap_bar_cols = dict(sorted(shap_dict.items(), key=lambda x: np.abs(x[1]), reverse=True,)[:n_cols])
         return kept_shap_bar_cols
