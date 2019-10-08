820a694761820fa7408878b76cc877a14f2ad7bb
Ludmila Exbrayat <ludmilaex@gmail.com>
2019-10-08 16:09:35 +0200

    updated based on Antoine's comments

--- trelawney/logreg_explainer.py
+++ trelawney/logreg_explainer.py
@@ -22,7 +22,7 @@ class LogRegExplainer(BaseExplainer):
         self.class_names = class_names
         self._model_to_explain = None
 
-    def fit(self, model: sklearn.base.BaseEstimator, , x_train: pd.DataFrame, y_train: pd.DataFrame): 
+    def fit(self, model: sklearn.base.BaseEstimator, x_train: pd.DataFrame, y_train: pd.DataFrame): 
         self._model_to_explain = model
         self._feature_names = x_train.columns
 
@@ -35,23 +35,23 @@ class LogRegExplainer(BaseExplainer):
         res = dict(zip(x_explain.columns, np.abs(self._model_to_explain.coef_[0])))
         return res
 
-    def _compute_odds_ratio(self);
-        res = pd.DataFrame('variables': self._feature_names, 
-                           'odds_ratio': np.exp(self._model_to_explain.coef_[0])).set_index('variables')
+    def _compute_odds_ratio(self):
+        res = pd.DataFrame({'variables': self._feature_names, 
+                                   'odds_ratio': np.exp(self._model_to_explain.coef_[0])}).set_index('variables')
         return res
 
-    def graph_odds_ratio(self, top: int = 10, ascending: bool = False):
+    def graph_odds_ratio(self, n_cols: Optional[int] = 10, ascending: bool = False):
         """
         returns a plot of the top k features, based on the magnitude of their odds ratio.
-        :top: number of features to plot
+        :n_cols: number of features to plot
         :ascending: order of the ranking of the magnitude of the coefficients
         """
-        dataset_to_plot = self._compute_odds_ratio().sort_values(['odds_ratio'], ascending=ascending).head(top)
+        dataset_to_plot = self._compute_odds_ratio().sort_values(['odds_ratio'], ascending=ascending).head(n_cols)
 
         fig, ax = plt.subplots(figsize=(10,10))
 
         sns.barplot(y=dataset_to_plot.index, x=dataset_to_plot['odds_ratio'], ax=ax, palette=sns.color_palette("RdBu", n_colors=7))
-        ax.set_title('Top %i variables, based on their odds ratio'%(top))
+        ax.set_title('Top %i variables, based on their odds ratio'%(n_cols))
         plt.show()
         return ax
 
